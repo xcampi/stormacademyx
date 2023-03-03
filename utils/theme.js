03004b6fd89b588f6c05c4e3687673e2a41debbc
Campi <76500461+xcampi@users.noreply.github.com>
2023-03-03 13:50:52 +0000

    Update theme.js

--- utils/theme.js
+++ utils/theme.js
@@ -3,11 +3,11 @@ import { extendTheme } from '@chakra-ui/react';
 const colors = {
 	brand: {
 		primary: {
-			100: '#67F858',
-			200: '#21C523',
-			300: '#009500',
-			400: '#006600',
-			500: '#003A00',
+			100: '#C4E344',
+			200: '#A3C320',
+			300: '#C5F014',
+			400: '#BBD15E',
+			500: '#8DA234',
 		},
 		secondary: '#313131',
 	},
